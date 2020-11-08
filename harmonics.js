cae0e4a85a28b731c9eb9dded64588d1df1919f3
Ross Wang <imagipioneer@gmail.com>
2020-11-08 13:09:36 -0800

    Add fundamental shifting; populate most of the scale display data
    Add buttons to shift fundamental octave.
    
    Populates scale display data except for intonation color.

--- harmonics.js
+++ harmonics.js
@@ -2,51 +2,104 @@ var maxGainAt = 220;
 var minScalePlaybackDuration = 1;
 var rampLength = .015;
 
+var register = 4;
 var audioCtx;
 
 function Player() {
   audioCtx ||= new (window.AudioContext || window.webkitAudioContext)();
 
-  this.oscillator = audioCtx.createOscillator();
-  this.gain = audioCtx.createGain();
+  var oscillator = audioCtx.createOscillator();
+  var gainNode = audioCtx.createGain();
 
-  this.oscillator.connect(this.gain);
-  this.gain.connect(audioCtx.destination);
+  oscillator.connect(gainNode);
+  gainNode.connect(audioCtx.destination);
 
-  this.length = 0;
-  this.gain.gain.setValueAtTime(0, audioCtx.currentTime);
+  var length = 0;
+  gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
 
   this.start = function () {
-    this.oscillator.start();
-    this.oscillator.stop(audioCtx.currentTime + this.length);
+    oscillator.start();
+    oscillator.stop(audioCtx.currentTime + length);
   };
 
   this.addTone = function (frequency, duration) {
     var gain = Math.min(1, maxGainAt / frequency);
-    var t = audioCtx.currentTime + this.length;
+    var t = audioCtx.currentTime + length;
 
-    this.oscillator.frequency.setValueAtTime(frequency, t);
-    this.gain.gain.linearRampToValueAtTime(gain, t + rampLength);
-    this.gain.gain.setValueAtTime(gain, t + duration - rampLength);
-    this.gain.gain.linearRampToValueAtTime(0, t + duration);
+    oscillator.frequency.setValueAtTime(frequency, t);
+    gainNode.gain.linearRampToValueAtTime(gain, t + rampLength);
+    gainNode.gain.setValueAtTime(gain, t + duration - rampLength);
+    gainNode.gain.linearRampToValueAtTime(0, t + duration);
 
-    this.length += duration;
+    length += duration;
     return this;
   };
 }
 
 function Scale() {
-  this.fundamental = $("#fundamental").val();
+  this.fundamental = $("#tonic").val() / Math.pow(2, register);
   this.degrees = $("#scale").val();
 
   this.equalTemperamentFrequency = function (degree) {
-    return this.fundamental * Math.pow(2, degree / this.degrees);
+    return this.fundamental * Math.pow(2, register + degree / this.degrees);
+  };
+
+  this.harmonicFrequency = function (harmonic) {
+    return this.fundamental * harmonic;
   }
+
+  this.toDegree = function (harmonic) {
+    return (Math.log2(harmonic) - register) * this.degrees;
+  };
+}
+
+function ScaleDegree(selector, scale, degree) {
+  var etf = scale.equalTemperamentFrequency(degree);
+  var absIntonation;
+
+  selector.find(".sd-degree").text((degree % scale.degrees) + 1);
+  selector.find(".sd-etf").text(Math.round(etf));
+
+  function formatIntonation(intonation) {
+    return intonation > 0 ? "+ " + intonation.toFixed(2).substr(1) :
+      intonation < 0 ? "- " + intonation.toFixed(2).substr(2) :
+        "± .00";
+  }
+
+  function factorHarmonic(harmonic) {
+    var exp = 0;
+    while (harmonic % 2 == 0) {
+      ++exp;
+      harmonic >>= 1;
+    }
+    return {
+      odd: harmonic,
+      exp: exp
+    };
+  }
+
+  this.addHarmonic = function (harmonic) {
+    var intonation = scale.toDegree(harmonic) - degree;
+
+    if (!(absIntonation < Math.abs(intonation))) {
+      absIntonation = Math.abs(intonation);
+      var factored = factorHarmonic(harmonic);
+
+      selector.find(".sd-intonation, .sd-harmonic-info").removeClass("d-none");
+      selector.removeClass("list-group-item-secondary list-group-item-success list-group-item-warning list-group-item-danger");
+
+      selector.find(".sd-harmonic").text(harmonic);
+      selector.find(".sd-intonation").text(formatIntonation(intonation));
+      selector.find(".sd-harmonic-odd").text(factored.odd);
+      selector.find(".sd-harmonic-exp").text(factored.exp);
+      selector.find(".sd-jif").text(Math.round(scale.harmonicFrequency(harmonic)) + " Hz");
+    }
+  };
 }
 
 $(function () {
-  $("#playFundamental").click(function () {
-    new Player().addTone(new Scale().fundamental, 1).start();
+  $("#playTonic").click(function () {
+    new Player().addTone($("#tonic").val(), 1).start();
   });
 
   $("#playScale").click(function () {
@@ -76,6 +129,12 @@ $(function () {
     updateScale();
   });
 
+  $(".shift-register .btn").click(function () {
+    register += $(this).data("registerShift");
+    $(".shift-register .btn[data-register-shift='-1']").attr("disabled", register == 0);
+    updateScale();
+  });
+
   var toneTemplate = $(".tones > *").detach();
 
   function updateScale() {
@@ -84,18 +143,21 @@ $(function () {
     var tones = $(".tones");
     tones.empty();
 
-    for (var i = 0; i <= scale.degrees; ++i) {
-      var ordinalDegree = (i % scale.degrees) + 1;
-      var etf = scale.equalTemperamentFrequency(i);
+    var scaleDegrees = [];
 
+    for (var i = 0; i <= scale.degrees; ++i) {
       var tone = toneTemplate.clone();
-      tone.find(".sd-intonation").text(ordinalDegree);
-      tone.find(".sd-etf").text(Math.round(etf) + " Hz");
+      scaleDegrees.push(new ScaleDegree(tone, scale, i));
       tones.append(tone);
+    }
+
+    var h0 = Math.pow(2, register);
+    for (var n = h0; n <= 2 * h0; ++n) {
+      scaleDegrees[Math.round(scale.toDegree(n))].addHarmonic(n);
     }
   }
 
-  $("#fundamental, #scale").change(updateScale);
+  $("#tonic, #scale").change(updateScale);
 
   updateScale();
 });
