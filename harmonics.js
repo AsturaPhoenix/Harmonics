97e82809beae8ddc363ec490ec1da3cc01ed3eb6
Ross Wang <imagipioneer@gmail.com>
2020-11-08 22:35:03 -0800

    Inverted to make harmonics primary, scale secondary
    The original program was centered around the harmonics rather than the scale, and the quality of coverage of the
    strongest harmonics is likely to be a more important metric for the quality of a scale temperament. This demo makes
    it easier to compare the coverage of different scale systems.

--- harmonics.js
+++ harmonics.js
@@ -1,19 +1,30 @@
-var maxGainAt = 220;
+var maxGainAt = 110;
+var finalGain = .5;
 var minScalePlaybackDuration = 1, toneDuration = 1;
 var rampLength = .015;
 
-var register = 4;
+var nHarmonics = 32;
 var audioCtx;
+var compressor;
 
-function Track(volume) {
-  volume ||= 1;
-  audioCtx ||= new (window.AudioContext || window.webkitAudioContext)();
+function Track() {
+  if (!audioCtx) {
+    audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+    compressor = audioCtx.createDynamicsCompressor();
+    var finalGainNode = audioCtx.createGain();
+
+    finalGainNode.gain.setValueAtTime(finalGain, 0);
+
+    compressor.connect(finalGainNode);
+    finalGainNode.connect(audioCtx.destination);
+  }
 
   var oscillator = audioCtx.createOscillator();
   var gainNode = audioCtx.createGain();
 
   oscillator.connect(gainNode);
-  gainNode.connect(audioCtx.destination);
+  gainNode.connect(compressor);
 
   var length = 0;
   gainNode.gain.setValueAtTime(0, audioCtx.currentTime);
@@ -24,7 +35,7 @@ function Track(volume) {
   };
 
   this.addTone = function (frequency, duration) {
-    var gain = Math.min(1, volume * maxGainAt / frequency);
+    var gain = Math.min(1, maxGainAt / frequency);
     var t = audioCtx.currentTime + length;
 
     oscillator.frequency.setValueAtTime(frequency, t);
@@ -38,11 +49,11 @@ function Track(volume) {
 }
 
 function Scale() {
-  this.fundamental = $("#tonic").val() / Math.pow(2, register);
+  this.fundamental = $("#fundamental").val();
   this.degrees = $("#scale").val();
 
-  this.equalTemperamentFrequency = function (degree) {
-    return this.fundamental * Math.pow(2, register + degree / this.degrees);
+  this.scaleFrequency = function (degree) {
+    return this.fundamental * Math.pow(2, degree / this.degrees);
   };
 
   this.harmonicFrequency = function (harmonic) {
@@ -50,93 +61,114 @@ function Scale() {
   }
 
   this.toDegree = function (harmonic) {
-    return (Math.log2(harmonic) - register) * this.degrees;
+    return Math.log2(harmonic) * this.degrees;
   };
 }
 
-function ScaleDegree(selector, scale, degree) {
-  var etf = scale.equalTemperamentFrequency(degree);
-  var jif;
-  var absIntonation;
+function factorHarmonic(harmonic) {
+  var exp = 0;
+  while (harmonic % 2 == 0) {
+    ++exp;
+    harmonic >>= 1;
+  }
+  return {
+    odd: harmonic,
+    exp: exp
+  };
+}
 
-  selector.find(".sd-degree").text((degree % scale.degrees) + 1);
-  selector.find(".sd-etf").text(Math.round(etf));
+function formatIntonation(intonation) {
+  return intonation > 0 ? "+ " + intonation.toFixed(2).substr(1) :
+    intonation < 0 ? "- " + intonation.toFixed(2).substr(2) :
+      "± .00";
+}
 
-  function formatIntonation(intonation) {
-    return intonation > 0 ? "+ " + intonation.toFixed(2).substr(1) :
-      intonation < 0 ? "- " + intonation.toFixed(2).substr(2) :
-        "± .00";
+function initHarmonicEntries(selector) {
+  selector.each(function (_, element) {
+    var selector = $(element);
+
+    var harmonic = selector.index() + 1;
+    var factored = factorHarmonic(harmonic);
+
+    selector.find(".he-harmonic").text(harmonic);
+    selector.find(".he-harmonic-odd").text(factored.odd);
+    selector.find(".he-harmonic-exp").text(factored.exp);
+
+    if (!$("#show-all-harmonics").prop("checked") && harmonic % 2 == 0) {
+      selector.hide();
+    }
+  });
+}
+
+function updateHarmonicEntries(selector, scale) {
+  selector.each(function (_, element) {
+    var selector = $(element);
+
+    var harmonic = selector.index() + 1;
+    var exactDegree = scale.toDegree(harmonic)
+    var degree = Math.round(exactDegree);
+    var intonation = exactDegree - degree;
+    var absIntonation = Math.abs(intonation);
+
+    var scaleFrequency = scale.scaleFrequency(degree);
+    var harmonicFrequency = scale.harmonicFrequency(harmonic);
+
+    selector.removeClass("list-group-item-secondary list-group-item-success list-group-item-warning list-group-item-danger");
+
+    if (absIntonation <= .1) {
+      selector.addClass("list-group-item-success");
+    } else if (absIntonation <= .2) {
+      selector.addClass("list-group-item-warning");
+    } else {
+      selector.addClass("list-group-item-danger");
+    }
+
+    selector.find(".he-degree").text((degree % scale.degrees) + 1);
+    selector.find(".he-intonation").text(formatIntonation(intonation));
+    selector.find(".he-sf").text(Math.round(scaleFrequency));
+    selector.find(".he-hf").text(Math.round(harmonicFrequency));
+
+    selector.data("scaleFrequency", scaleFrequency);
+    selector.data("harmonicFrequency", harmonicFrequency);
+  });
+}
+
+function playHarmonicEntries(selector) {
+  if ($("#play-scale-tone").prop("checked")) {
+    selector.each(function (_, element) {
+      new Track().addTone($(element).data("scaleFrequency"), toneDuration).start();
+    });
   }
 
-  function factorHarmonic(harmonic) {
-    var exp = 0;
-    while (harmonic % 2 == 0) {
-      ++exp;
-      harmonic >>= 1;
-    }
-    return {
-      odd: harmonic,
-      exp: exp
-    };
+  if ($("#play-harmonic").prop("checked")) {
+    selector.each(function (_, element) {
+      new Track().addTone($(element).data("harmonicFrequency"), toneDuration).start();
+    });
   }
-
-  this.addHarmonic = function (harmonic) {
-    var intonation = scale.toDegree(harmonic) - degree;
-
-    if (!(absIntonation < Math.abs(intonation))) {
-      absIntonation = Math.abs(intonation);
-      var factored = factorHarmonic(harmonic);
-      jif = scale.harmonicFrequency(harmonic);
-
-      selector.find(".sd-intonation, .sd-harmonic-info").removeClass("d-none");
-      selector.removeClass("list-group-item-secondary list-group-item-success list-group-item-warning list-group-item-danger");
-
-      if (absIntonation <= .1) {
-        selector.addClass("list-group-item-success");
-      } else if (absIntonation <= .2) {
-        selector.addClass("list-group-item-warning");
-      } else {
-        selector.addClass("list-group-item-danger");
-      }
-
-      selector.find(".sd-harmonic").text(harmonic);
-      selector.find(".sd-intonation").text(formatIntonation(intonation));
-      selector.find(".sd-harmonic-odd").text(factored.odd);
-      selector.find(".sd-harmonic-exp").text(factored.exp);
-      selector.find(".sd-jif").text(Math.round(jif) + " Hz");
-    }
-  };
-
-  this.isSelected = function () {
-    return selector.hasClass("active");
-  };
-
-  this.play = function (volume) {
-    if ($("#scale-equal").prop("checked")) {
-      new Track(.5 * volume).addTone(etf, toneDuration).start();
-    }
-    if (jif !== undefined && $("#scale-just").prop("checked")) {
-      new Track(.5 * volume).addTone(jif, toneDuration).start();
-    }
-  };
 }
 
 $(function () {
-  $("#playTonic").click(function () {
-    new Track().addTone($("#tonic").val(), toneDuration).start();
+  $("#playFundamental").click(function () {
+    new Track().addTone($("#fundamental").val(), toneDuration).start();
   });
 
   $("#playScale").click(function () {
     var scale = new Scale();
+    while (scale.fundamental < 330) {
+      scale.fundamental *= 2;
+    }
+    while (scale.fundamental > 660) {
+      scale.fundamental /= 2;
+    }
 
     var toneDuration = Math.max(.125, minScalePlaybackDuration / scale.degrees);
 
     var player = new Track();
     for (var i = 0; i < scale.degrees; ++i) {
-      player.addTone(scale.equalTemperamentFrequency(i), toneDuration);
+      player.addTone(scale.scaleFrequency(i), toneDuration);
     }
     for (var i = scale.degrees; i >= 0; --i) {
-      player.addTone(scale.equalTemperamentFrequency(i), toneDuration);
+      player.addTone(scale.scaleFrequency(i), toneDuration);
     }
     player.start();
   });
@@ -147,56 +179,61 @@ $(function () {
     }
   });
 
+  $("#adjustFundamental .btn").click(function () {
+    var multiplier = $(this).data("fundamentalMultiplier");
+    $("#fundamental").val(function (_, f) {
+      return f *= multiplier;
+    });
+    updateScale();
+
+    playSelected() || $("#playFundamental").click();
+  });
+
   $("#scalePresets button").click(function () {
     $("#scale").val($(this).text());
-    $("#playScale").click();
     updateScale();
+
+    playSelected() || $("#playScale").click();
   });
 
-  $(".shift-register .btn").click(function () {
-    register += $(this).data("registerShift");
-    $(".shift-register .btn[data-register-shift='-1']").attr("disabled", register == 0);
-    updateScale();
-  });
-
-  var toneTemplate = $(".tones > *").detach();
-  var scaleDegrees;
+  var entryTemplate = $(".harmonics > *").detach();
 
   function updateScale() {
     var scale = new Scale();
-
-    var tones = $(".tones");
-    tones.empty();
-
-    scaleDegrees = [];
-
-    for (var i = 0; i <= scale.degrees; ++i) {
-      var tone = toneTemplate.clone();
-      scaleDegrees.push(new ScaleDegree(tone, scale, i));
-      tones.append(tone);
-    }
-
-    var h0 = Math.pow(2, register);
-    for (var n = h0; n <= 2 * h0; ++n) {
-      scaleDegrees[Math.round(scale.toDegree(n))].addHarmonic(n);
-    }
+    updateHarmonicEntries($(".harmonics > *"), scale);
   }
 
-  $("#tonic, #scale").change(updateScale);
+  $("#fundamental, #scale").change(updateScale);
 
+  for (var i = 0; i < nHarmonics; ++i) {
+    $(".harmonics").append(entryTemplate.clone());
+  }
+  initHarmonicEntries($(".harmonics > *"));
   updateScale();
 
   function playSelected() {
-    var selected = scaleDegrees.filter(s => s.isSelected());
-    for (var scaleDegree of selected) {
-      scaleDegree.play(1 / Math.sqrt(selected.length));
-    }
+    var selected = $(".harmonics > *").filter(".active");
+    playHarmonicEntries(selected);
+    return selected.length > 0;
   }
 
-  $(".tones").click(function (e) {
+  $(".harmonics").click(function (e) {
     $(e.target).closest(".list-group-item").toggleClass("active");
     playSelected();
   });
 
-  $("#scale-equal, #scale-just").change(playSelected);
+  $("#play-scale-tone, #play-harmonic").change(function () { playSelected(); });
+
+  $("#show-all-harmonics").change(function () {
+    var evens = $(".harmonics > :odd"); // because 0-indexed
+    var label = $(this).next();
+    if (this.checked) {
+      evens.show();
+      label.text("All harmonics");
+    } else {
+      evens.hide();
+      evens.removeClass("active");
+      label.text("Odd harmonics only");
+    }
+  });
 });
